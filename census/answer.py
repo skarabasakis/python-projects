9b80ac6c90e47973b72eafefe951e18eb195327d
Stelios Karabasakis <skarabasakis@gmail.com>
2020-12-26 23:57:31 +0200

    Split census into modules

---
+++ census/answer.py
@@ -0,0 +1,16 @@
+def format_prompt(prompt):
+    return f'{prompt}: '
+
+def number(prompt):
+    x = input(format_prompt(prompt))
+    return int(x)
+
+def string(prompt):
+    x = input(format_prompt(prompt))
+    return x
+
+def select(prompt, options):
+    options_str = ', '.join([f"({k}) {v}" for k, v in options.items()])
+    selections = options.keys()
+    x = input(format_prompt(f"{prompt}: {options_str}. ({'/'.join(selections)})"))
+    return options.get(x)
--- census/main.py
+++ census/main.py
@@ -1,47 +1,17 @@
-def display_underlined(text, symbol = '-'):
-    print()
-    print(text)
-    print(symbol * len(text))
+import answer
+import display
 
-def display_bordered(text, symbol = '*'):
-    print()
-    print(symbol * (len(text) + 4))
-    print(symbol, text, symbol)
-    print(symbol * (len(text) + 4))
-    print()
+display.bordered('Census Questionnaire')
 
-def format_prompt(prompt):
-    return f'{prompt}: '
+members_count = answer.number('How many people live in your household?')
+members = []
 
-def answer_number(prompt):
-    x = input(format_prompt(prompt))
-    return int(x)
+for i in range(members_count):
+    display.underlined(f'Member {i+1}')
+    members.append({
+        'Name': f'''{answer.string('Last Name')}, {answer.string('First Name')}''',
+        'Sex': answer.select('Sex', {'m': 'Male', 'f': 'Female'}),
+        'Employment': answer.select('Employment Status', {'a': 'Full-time', 'b': 'Part-time', 'c': 'Unemployed'})
+    })
 
-def answer_string(prompt):
-    x = input(format_prompt(prompt))
-    return x
-
-def answer_select(prompt, options):
-    options_str = ', '.join([f"({k}) {v}" for k, v in options.items()])
-    selections = options.keys()
-    x = input(format_prompt(f"{prompt}: {options_str}. ({'/'.join(selections)})"))
-    return options.get(x)
-
-def main():
-    display_bordered('Census Questionnaire')
-
-    members_count = answer_number('How many people live in your household?')
-    members = []
-
-    for i in range(members_count):
-        display_underlined(f'Member {i+1}')
-        members.append({
-            'First Name': answer_string('First Name'),
-            'Last Name': answer_string('Last Name'),
-            'Sex': answer_select('Sex', {'m': 'Male', 'f': 'Female'}),
-            'Employment': answer_select('Employment Status', {'a': 'Full-time', 'b': 'Part-time', 'c': 'Unemployed'})
-        })
-
-    print(members)
-
-main()
+print(members)
