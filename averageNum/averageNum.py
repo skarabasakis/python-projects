c65776175d2ca771f34dce397739a259831d45a9
Dimitris Marakomihelakis <dmarakom@gmail.com>
2021-02-19 20:56:14 +0200

    averageNum.py: average of a set of numbers

---
+++ averageNum/averageNum.py
@@ -0,0 +1,17 @@
+lengthNum = int(input("Enter the length of numbers you are willing to use (i.e. 3): "))
+
+nums = {}
+
+for i in range(lengthNum):
+    string = "Enter number " + str(i+1) + ": "
+    num = int(input(string))
+    nums["num{0}".format(i)] = num
+    i += 1
+
+sum = 0
+for num in nums:
+    sum += nums[num]
+
+average = sum / lengthNum
+
+print(f"\nThe average of the {lengthNum} numbers given is {average} .")
