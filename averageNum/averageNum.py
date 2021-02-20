34d43743a0e52ec82a729b40a9d1974933151ef3
Dimitris Marakomihelakis <dmarakom@gmail.com>
2021-02-20 14:08:17 +0200

    Add comments

--- averageNum/averageNum.py
+++ averageNum/averageNum.py
@@ -1,17 +1,17 @@
-lengthNum = int(input("Enter the length of numbers you are willing to use (i.e. 3): "))
+lengthNum = int(input("Enter the length of numbers you are willing to use (i.e. 3): ")) # Get the length of numbers the user wishes to use
 
-nums = {}
+nums = {} # Create a dictionary to store the numbers
 
 for i in range(lengthNum):
     string = "Enter number " + str(i+1) + ": "
-    num = int(input(string))
-    nums["num{0}".format(i)] = num
+    num = int(input(string)) # Get each number by the user
+    nums["num{0}".format(i)] = num # Store each number to the "nums" dictionary
     i += 1
 
 sum = 0
 for num in nums:
-    sum += nums[num]
+    sum += nums[num] # Find the sum of each number in the "nums" dictionary
 
-average = sum / lengthNum
+average = sum / lengthNum # (num1 + num2 ... ) / the length of all the numbers (lengthNum)
 
-print(f"\nThe average of the {lengthNum} numbers given is {average:.2f} .")
+print(f"\nThe average of the {lengthNum} numbers given is {average:.2f} .") # Use ':.2f' To round the result to 2 numbers after the decimal point.
