79fa214f621d3896fada24d73ee0fad0236bfd46
Dimitris Marakomihelakis <dmarakom@gmail.com>
2023-06-26 12:38:10 +0300

    better menu

--- e-shop/assets/js/navbar.js
+++ e-shop/assets/js/navbar.js
@@ -1,41 +1,60 @@
-// MENU
+// MENU & SEARCH
 const menuBtn = document.querySelector(".menu-icon span");
-    const searchBtn = document.querySelector(".search-icon");
-    const cancelBtn = document.querySelector(".cancel-icon");
-    const items = document.querySelector(".nav-items");
-    const form = document.querySelector("form");
-    menuBtn.onclick = ()=>{
-      items.classList.add("active");
-      menuBtn.classList.add("hide");
-      searchBtn.classList.add("hide");
-      cancelBtn.classList.add("show");
-    }
-    cancelBtn.onclick = ()=>{
-      items.classList.remove("active");
-      menuBtn.classList.remove("hide");
-      searchBtn.classList.remove("hide");
-      cancelBtn.classList.remove("show");
-      form.classList.remove("active");
-      cancelBtn.style.color = "#ff3d00";
-    }
-    searchBtn.onclick = ()=>{
-      form.classList.add("active");
-      searchBtn.classList.add("hide");
-      cancelBtn.classList.add("show");
-    }
+const searchBtn = document.querySelector(".search-icon");
+const cancelBtn = document.querySelector(".cancel-icon");
+const items = document.querySelector(".nav-items");
+const form = document.querySelector("form");
+menuBtn.onclick = () => {
+  items.classList.add("active");
+  menuBtn.classList.add("hide");
+  searchBtn.classList.add("hide");
+  cancelBtn.classList.add("show");
+}
+cancelBtn.onclick = () => {
+  items.classList.remove("active");
+  menuBtn.classList.remove("hide");
+  searchBtn.classList.remove("hide");
+  cancelBtn.classList.remove("show");
+  form.classList.remove("active");
+  cancelBtn.style.color = "#ff3d00";
+}
+searchBtn.onclick = () => {
+  form.classList.add("active");
+  searchBtn.classList.add("hide");
+  cancelBtn.classList.add("show");
+}
+
+
+// close the menu when user clicks outside of it
+const closeMenu = () => {
+  items.classList.remove("active");
+  menuBtn.classList.remove("hide");
+  searchBtn.classList.remove("hide");
+  cancelBtn.classList.remove("show");
+  form.classList.remove("active");
+  cancelBtn.style.color = "#ff3d00";
+};
+
+// Event listener to close the menu when clicked outside
+document.addEventListener("click", (event) => {
+  const isClickInsideMenu = items.contains(event.target);
+  const isClickOnMenuButton = menuBtn.contains(event.target);
+  if (!isClickInsideMenu && !isClickOnMenuButton) {
+    closeMenu();
+  }
+});
 
 
 // BORDER ON SCROLL
-    window.addEventListener('scroll', function() {
-      var navbar = document.querySelector('nav');
-      var scrollPosition = window.scrollY;
-    
-      // Set the scroll position at which we want to add the border
-      var scrollThreshold = window.innerHeight * 0.62; // 75% of viewport height, around end of hero header
-      if (scrollPosition >= scrollThreshold) {
-        navbar.classList.add('nav-border');
-      } else {
-        navbar.classList.remove('nav-border');
-      }
-    });
-    
+window.addEventListener('scroll', function () {
+  var navbar = document.querySelector('nav');
+  var scrollPosition = window.scrollY;
+
+  // Set the scroll position at which we want to add the border
+  var scrollThreshold = window.innerHeight * 0.62; // 75% of viewport height, around end of hero header
+  if (scrollPosition >= scrollThreshold) {
+    navbar.classList.add('nav-border');
+  } else {
+    navbar.classList.remove('nav-border');
+  }
+});
