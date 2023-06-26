5304a78018b404c1279016128962d0b377904693
Dimitris Marakomihelakis <dmarakom@gmail.com>
2023-06-26 13:48:31 +0300

    footer & complete landing page

---
+++ e-shop/assets/js/currentYear.js
@@ -0,0 +1,5 @@
+
+// Auto year change in copyright text (footer)
+const currentYear = new Date().getFullYear();
+const yearElement = document.getElementById("currentYear");
+yearElement.textContent = currentYear;
