eba235ba383a102619374cb5ed41d12dc37a80fc
Dimitris Marakomihelakis <dmarakom@gmail.com>
2023-07-25 14:56:01 +0300

    work on checkout TODO: - Figure out a way to display only the "PayPal" Button - Make responsive (maybe hide the card mockup)

---
+++ e-shop/assets/js/inputToCardMockup.js
@@ -0,0 +1,9 @@
+/* COPY INPUT VALUES TO CARD MOCKUP */
+const bounds = document.querySelectorAll('[data-bound]');
+
+for(let i = 0; i < bounds.length; i++) {
+  const targetId = bounds[i].getAttribute('data-bound');
+  const defValue = bounds[i].getAttribute('data-def');
+  const targetEl = document.getElementById(targetId);
+  bounds[i].addEventListener('keyup', () => targetEl.innerText = bounds[i].value || defValue );
+}
